0487228d9978a4baf0bd685c4a9063171f6aaf4c
MatheusPrusch77 <143458611+MatheusPrusch77@users.noreply.github.com>
2023-10-12 19:11:58 -0300

    Update exemplo01.py

--- Aula04 - piva/exemplo01.py
+++ Aula04 - piva/exemplo01.py
@@ -1,4 +1,4 @@
 base = int(input("Entre com o valor da base: "))
-altura = int(input("Entre com o valor da ltura: "))
+altura = int(input("Entre com o valor da altura: "))
 area = base * altura
 print("A base é=", base, "*", altura, "Logo, a area é igual a= ", area)
