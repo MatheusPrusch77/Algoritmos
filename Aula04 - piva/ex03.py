0ddcbdad267ec6642a1bf89ce3bba68af34cac2c
MatheusPrusch77 <143458611+MatheusPrusch77@users.noreply.github.com>
2023-10-12 19:11:04 -0300

    Update ex03.py

--- Aula04 - piva/ex03.py
+++ Aula04 - piva/ex03.py
@@ -2,4 +2,4 @@ salario = float(input("Digite seu salário: "))
 
 alterado = salario * 1.25
 
-print("Seu salário é", salario, f"Devido a alteração de 25%, seu salario subiu para: {alterado: 8.2f} ")
+print(f"Seu salário é", salario, f"Devido a alteração de 25%, seu salario subiu para: {alterado: 8.2f} ")
