dcd6df91b7ae460b0a499cd283f83616c089c0c2
MatheusPrusch77 <143458611+MatheusPrusch77@users.noreply.github.com>
2023-10-13 13:39:23 -0300

    ex01.py

--- Aula05 - piva/ex01.py
+++ Aula05 - piva/ex01.py
@@ -7,7 +7,7 @@ if n1 == n2:
 else:
 
     if n1 > n2:
-    print(f"O numero {n1} é maior")
+        print(f"O numero {n1} é maior")
     else:
-    print(f"O numero {n1} é menor que o {n2}")
+        print(f"O numero {n1} é menor que o {n2}")
 
